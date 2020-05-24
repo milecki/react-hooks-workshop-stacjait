b92d52906628d89cf2a5ec393455785d42248235
milecki <m.kulecki@gmail.com>
2020-05-24 10:23:18 +0200

    adds CounterHooks comp with +, -, and reset functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './App.css';
 import Counter from './Counter';
+import CounterHooks from './CounterHooks';
 
 function App() {
   return (
     <div className="App">
       <h1>Blog Michała - warsztat React Hooks</h1>
       <Counter />
+      <CounterHooks />
     </div>
   );
 }
---
+++ src/CounterHooks.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+function CounterHooks() {
+  const initialValue = 10;
+  const [counterValue, setCounterValue] = useState(initialValue);
+
+  const incrementCounter = () => {
+    setCounterValue(counterValue + 1);
+  };
+
+  const decrementCounter = () => {
+    setCounterValue(counterValue - 1);
+  };
+
+  const resetCounter = () => {
+    setCounterValue(initialValue);
+  };
+
+  return (
+    <div>
+      <h2>Counter Hooks:</h2>
+      <div>{counterValue}</div>
+      <button onClick={incrementCounter}>+</button>
+      <button onClick={resetCounter}>reset</button>
+      <button onClick={decrementCounter}>-</button>
+    </div>
+  );
+}
+
+export default CounterHooks;
