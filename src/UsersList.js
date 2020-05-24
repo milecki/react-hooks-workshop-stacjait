e2e42d8db2f234a2e0981db16ff9b092cb0e9d02
milecki <m.kulecki@gmail.com>
2020-05-24 13:50:56 +0200

    adds error handling to UsersList

--- src/UsersList.js
+++ src/UsersList.js
@@ -1,22 +1,34 @@
 import React, { useState, useEffect } from 'react';
 
 function UsersList() {
-  const [users, setUsers] = useState([]);
+  const [usersData, setUsers] = useState([]);
+  const [usersLoading, setUsersLoading] = useState(false);
+  const [usersError, setUsersError] = useState(false);
 
   useEffect(() => {
+    setUsersLoading(true);
     fetch('http://jsonplaceholder.typicode.com/users')
       .then((res) => res.json())
-      .then((json) => setUsers(json));
+      .then((json) =>
+        setTimeout(() => {
+          setUsersLoading(false);
+          setUsers(json);
+        }, 3000)
+      )
+      .catch((err) => {
+        setUsersLoading(false);
+        setUsersError(true);
+      });
   }, []);
+
+  const renderUsers = () => usersData.map((user) => <div>{user.name}</div>);
 
   return (
     <>
-      <h2>Users List</h2>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.username}</li>
-        ))}
-      </ul>
+      <h2>Użytkownicy</h2>
+      {usersLoading && <div> Ładowanie danych</div>}
+      {renderUsers()}
+      {usersError && <div>Błąd pobierania danych</div>}
     </>
   );
 }
